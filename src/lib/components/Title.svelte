92593d656423cf1899c5d0bd499716cab95dae72
Goldentoaste <rayg2375@gmail.com>
2023-03-04 19:07:37 -0800

    asd

--- src/lib/components/Title.svelte
+++ src/lib/components/Title.svelte
@@ -1,53 +1,44 @@
 <script lang="ts">
-  import { onMount } from "svelte";
-  import { fade } from "svelte/transition";
+    import { onMount } from 'svelte';
+    import { fade } from 'svelte/transition';
 
-  export let style = "";
+    export let style = '';
 
-  const d = new Date();
-  let date = `Today is ${d.toLocaleDateString("en-US", {
-    month: "short",
-    day: "numeric",
-    year: "numeric",
-  })}`;
+    const d = new Date();
+    let date = `Today is ${d.toLocaleDateString('en-US', {
+        month: 'short',
+        day: 'numeric',
+        year: 'numeric',
+    })}`;
 
-  let message = "What will your fortune hold?";
+    let message = 'What will your fortune hold?';
 
-  let displayDate = true;
+    let displayDate = true;
 
-  onMount(() => {
-    setInterval(() => {
-      displayDate = !displayDate;
-    }, 4000);
-  });
+    onMount(() => {
+        setInterval(() => {
+            displayDate = !displayDate;
+        }, 4000);
+    });
 </script>
 
-<div
-  class="title-div"
-  {style}
->
-  {#if displayDate}
-    <h5
-      in:fade={{ duration: 1000, delay: 1000 }}
-      out:fade={{ duration: 1000 }}>{date}</h5
-    >
-  {:else}
-    <h5
-      in:fade={{ duration: 1000, delay: 1000 }}
-      out:fade={{ duration: 1000 }}>{message}</h5
-    >
-  {/if}
+<div class="title-div" {style}>
+    {#if displayDate}
+        <h4 in:fade={{ duration: 1000, delay: 1000 }} out:fade={{ duration: 1000 }}>{date}</h4>
+    {:else}
+        <h4 in:fade={{ duration: 1000, delay: 1000 }} out:fade={{ duration: 1000 }}>{message}</h4>
+    {/if}
 </div>
 
 <style>
-  h5 {
-    position: absolute;
-  }
+    h4 {
+        position: absolute;
+    }
 
-  div {
-    height: 20%;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-  }
+    div {
+        height: 20%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
 </style>
